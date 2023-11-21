a87a1f3087bee07dba7e391ff0cd438478f8cf35
guilhermemonteiro15JS <guilherme.monteiro@mindswap.academy>
2023-11-21 15:57:14 +0000

    navbar with gradient button and new file for stats

--- pokedex/src/components/NavBar/styled.js
+++ pokedex/src/components/NavBar/styled.js
@@ -9,7 +9,11 @@ export const NavBarStyle = styled.div`
 `;
 
 export const Button = styled.button`
-  background-color: ${({ isSelected }) => (isSelected ? "#305e8c" : "#4d9de0")};
+  background: ${({ isSelected }) =>
+    isSelected
+    ? "linear-gradient(90deg, #305e8c 6%, #4d9de0 25%, #6f42c1 45%, #007bff 55%, #0012cc 94%)"
+    : "linear-gradient(90deg, #00bfff 6%, #87ceeb 25%, #1e90ff 45%, #4682b4 55%, #4169e1 94%)"
+};
   color: white;
   border: 1px;
   padding: 10px 20px;
@@ -21,8 +25,7 @@ export const Button = styled.button`
   gap: 20px;
   font-size: 16px;
   cursor: pointer;
-  transition: background-color 0.3s, box-shadow 0.3s;
-  gap: 10px;
+  transition: background 0.3s, box-shadow 0.3s;
   box-shadow: ${({ isSelected }) => (isSelected ? "0 0 10px rgba(0, 0, 0, 0.5)" : "none")};
 
   &:active {
@@ -41,5 +44,5 @@ justify-content: start;
 export const DivButton = styled.div`
 display: flex;
 flex-direction: row;
-
+gap: 10px;
 `; 
