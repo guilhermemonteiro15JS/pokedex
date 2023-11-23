0cb234a9ad375b4ca7d682b23652c47e6e63761d
guilhermemonteiro15JS <guilherme.monteiro@mindswap.academy>
2023-11-23 19:18:47 +0000

    include the new images for type

--- pokedex/src/components/Right/PokeDetails/SourceForType/SourceForType.js
+++ pokedex/src/components/Right/PokeDetails/SourceForType/SourceForType.js
@@ -1,41 +1,41 @@
 export const getTypeImage = (type) =>{
     switch (type) {
         case 'water':
-          return process.env.PUBLIC_URL +'bk-water.png';
+          return process.env.PUBLIC_URL +'./images_Type/water.png';
         case 'rock':
-          return process.env.PUBLIC_URL +'bk-rock.png';
+          return process.env.PUBLIC_URL +'./images_Type/rock.png';
         case 'bug':
-            return process.env.PUBLIC_URL +'bk-bug.png';
+            return process.env.PUBLIC_URL +'./images_Type/bug.png';
         case 'dark':
-            return process.env.PUBLIC_URL +'bk-dark.png';
+            return process.env.PUBLIC_URL +'./images_Type/dark.png';
         case 'dragon':
-            return process.env.PUBLIC_URL +'bk-dragon.png';
+            return process.env.PUBLIC_URL +'./images_Type/dragon.png';
         case 'electric':
-            return process.env.PUBLIC_URL +'bk-electric.png';
+            return process.env.PUBLIC_URL +'./images_Type/electric.png';
         case 'fairy':
-          return process.env.PUBLIC_URL +'bk-fairy.png';
+          return process.env.PUBLIC_URL +'./images_Type/fairy.png';
         case 'fight':
-          return process.env.PUBLIC_URL +'bk-fight.png';
+          return process.env.PUBLIC_URL +'./images_Type/fight.png';
         case 'fire':
-            return process.env.PUBLIC_URL +'bk-fire.png';
+            return process.env.PUBLIC_URL +'./images_Type/fire.png';
         case 'flying':
-            return process.env.PUBLIC_URL +'bk-flying.png';
+            return process.env.PUBLIC_URL +'./images_Type/flying.png';
         case 'ghost':
-            return process.env.PUBLIC_URL +'bk-ghost.png';
+            return process.env.PUBLIC_URL +'./images_Type/ghost.png';
         case 'grass':
-            return process.env.PUBLIC_URL +'bk-grass.png';
+            return process.env.PUBLIC_URL +'./images_Type/grass.png';
         case 'ground':
-            return process.env.PUBLIC_URL +'bk-ground.png';
+            return process.env.PUBLIC_URL +'./images_Type/ground.png';
         case 'ice':
-            return process.env.PUBLIC_URL +'bk-ice.png';
+            return process.env.PUBLIC_URL +'./images_Type/ice.png';
         case 'normal':
-            return process.env.PUBLIC_URL +'bk-normal.png';
+            return process.env.PUBLIC_URL +'./images_Type/normal.png';
         case 'poison':
-            return process.env.PUBLIC_URL +'bk-poison.png';
+            return process.env.PUBLIC_URL +'./images_Type/poison.png';
         case 'psychic':
-            return process.env.PUBLIC_URL +'bk-psychic.png';
+            return process.env.PUBLIC_URL +'./images_Type/psychic.png';
         case 'steel':
-            return process.env.PUBLIC_URL +'bk-steel.png';    
+            return process.env.PUBLIC_URL +'./images_Type/steel.png';    
 
         default:
           return ''; 
