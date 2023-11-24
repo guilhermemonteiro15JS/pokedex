1ea1484cea4f8ad3ca28f5c744318c3a3ac4b8fc
guilhermemonteiro15JS <guilherme.monteiro@mindswap.academy>
2023-11-24 16:32:23 +0000

    include style on button' filter

--- pokedex/src/components/Left/Filter/Filter.js
+++ pokedex/src/components/Left/Filter/Filter.js
@@ -1,53 +1,26 @@
-/* import React, { useState } from "react";
-import { PokeFilterUl, InfoFilter } from "./styled";
-
-const Filter = (pokemon) => {
-  const [isselected, setisselected] = useState(null);
-
-  const handleTypeClick = (type) => {
-    
-    setisselected(type);
-  };
-
-  const types = pokemon.types || [];
-
-  return (
-    <div>
-         <div>
-        {pokemon.types.map((type, index) => (
-          <button
-            key={index}
-            onClick={() => handleTypeClick(type.type.name)}
-            className={isselected === type.type.name ? "active" : ""}
-          >
-            {type.type.name}
-          </button>
-        ))}
-      </div>
-
-      <PokeFilterUl>
-        {pokemon.types
-          .filter((type) => !isselected || type.type.name === isselected)
-          .map((type, index) => (
-            <li key={index}>
-              {type.type.name}
-              <InfoFilter>
-                <div>
-                  <span>{type.type.name}</span>
-                </div>
-                <button />
-              </InfoFilter>
-            </li>
-          ))}
-      </PokeFilterUl>
-    </div>
-  );
-};
-
-export default Filter;
- */
-
 import { React } from "react";
+import {
+  CountainerDiv,
+  Row,
+  ButtonNormal,
+  ButtonBug,
+  ButtonDragon,
+  ButtonDark,
+  ButtonElectric,
+  ButtonFairy,
+  ButtonFire,
+  ButtonFighting,
+  ButtonFlying,
+  ButtonGhost,
+  ButtonGrass,
+  ButtonGround,
+  ButtonIce,
+  ButtonPoison,
+  ButtonPsychic,
+  ButtonRock,
+  ButtonSteel,
+  ButtonWater,
+} from "./styled";
 
 const Filter = ({ handleRegion }) => {
   const handleType = (type) => {
@@ -57,27 +30,45 @@ const Filter = ({ handleRegion }) => {
   };
 
   return (
-    <div>
-      Filter
-      <button onClick={() => handleType("normal")}>Normal</button>
-      <button onClick={() => handleType("fire")}>Fire</button>
-      <button onClick={() => handleType("grass")}>Grass</button>
-      <button onClick={() => handleType("water")}>Water</button>
-      <button onClick={() => handleType("bug")}>Bug</button>
-      <button onClick={() => handleType("dark")}>Dark</button>
-      <button onClick={() => handleType("dragon")}>Dragon</button>
-      <button onClick={() => handleType("electric")}>electric</button>
-      <button onClick={() => handleType("fairy")}>Fairy</button>
-      <button onClick={() => handleType("fighting")}>Fighting</button>
-      <button onClick={() => handleType("flying")}>Flying</button>
-      <button onClick={() => handleType("ghost")}>Ghost</button>
-      <button onClick={() => handleType("ground")}>Ground</button>
-      <button onClick={() => handleType("ice")}>Ice</button>
-      <button onClick={() => handleType("psychic")}>Psychic</button>
-      <button onClick={() => handleType("poison")}>Poison</button>
-      <button onClick={() => handleType("rock")}>Rock</button>
-      <button onClick={() => handleType("steel")}>Steel</button>
-    </div>
+    <CountainerDiv>
+      <h2>Filter:</h2>
+      <Row>
+      <ButtonNormal onClick={() => handleType("normal")}></ButtonNormal>
+      <ButtonFire onClick={() => handleType("fire")}></ButtonFire>
+      </Row>
+      <Row>
+      <ButtonGrass onClick={() => handleType("grass")}></ButtonGrass>
+      <ButtonWater onClick={() => handleType("water")}></ButtonWater>
+      </Row>
+      <Row>
+      <ButtonBug onClick={() => handleType("bug")}></ButtonBug>
+      <ButtonDark onClick={() => handleType("dark")}></ButtonDark>
+      </Row>
+      <Row>
+      <ButtonDragon onClick={() => handleType("dragon")}></ButtonDragon>
+      <ButtonElectric onClick={() => handleType("electric")}></ButtonElectric>
+      </Row>
+      <Row>
+      <ButtonFairy onClick={() => handleType("fairy")}></ButtonFairy>
+      <ButtonFighting onClick={() => handleType("fighting")}></ButtonFighting>
+      </Row>
+      <Row>
+      <ButtonFlying onClick={() => handleType("flying")}></ButtonFlying>
+      <ButtonGhost onClick={() => handleType("ghost")}></ButtonGhost>
+      </Row>
+      <Row>
+      <ButtonGround onClick={() => handleType("ground")}></ButtonGround>
+      <ButtonIce onClick={() => handleType("ice")}></ButtonIce>
+      </Row>
+      <Row>
+      <ButtonPsychic onClick={() => handleType("psychic")}></ButtonPsychic>
+      <ButtonPoison onClick={() => handleType("poison")}></ButtonPoison>
+      </Row>
+      <Row>
+      <ButtonRock onClick={() => handleType("rock")}></ButtonRock>
+      <ButtonSteel onClick={() => handleType("steel")}></ButtonSteel>
+      </Row>
+    </CountainerDiv>
   );
 };
 
